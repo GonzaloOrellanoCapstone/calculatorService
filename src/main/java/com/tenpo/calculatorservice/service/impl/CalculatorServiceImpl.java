b55ffbf3fa7017cb513747c5bf0b17e875da7b27
GonzaloOrellanoCapstone <gonzalo.orellano@capstoneside.com>
2022-10-29 22:03:49 -0300

    Fix calculate service

--- src/main/java/com/tenpo/calculatorservice/service/impl/CalculatorServiceImpl.java
+++ src/main/java/com/tenpo/calculatorservice/service/impl/CalculatorServiceImpl.java
@@ -3,23 +3,12 @@ package com.tenpo.calculatorservice.service.impl;
 import com.tenpo.calculatorservice.service.CalculatorService;
 import org.springframework.stereotype.Service;
 
-import java.util.Random;
-
 @Service
 public class CalculatorServiceImpl implements CalculatorService {
 
     @Override
     public Double getPercentage(int firstValue, int secondValue) {
         Double addValues = Double.valueOf(firstValue + secondValue);
-        return addValues + calculatePercentage(addValues, randomNumber());
-    }
-
-    private int randomNumber() {
-        Random randI = new Random();
-        return randI.nextInt(100) + 1;
-    }
-
-    private double calculatePercentage(double obtained, double total) {
-        return obtained * 100 / total;
+        return addValues + ( (addValues * addValues) / 100);
     }
 }
